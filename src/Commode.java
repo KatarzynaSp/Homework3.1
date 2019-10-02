8ca285c2e0a38be99aa9dbd991f1810517af27db
Katarzyna <katarzyna.panek111@gmail.com>
2019-10-02 23:02:59 +0200

    Dodałam konstruktor (punkt 1.2) .

--- src/Commode.java
+++ src/Commode.java
@@ -2,4 +2,10 @@ public class Commode {
     String name;
     String color;
     double price;
+
+    public Commode (String n, String c, double p) {
+        name = n;
+        color = c;
+        price = p;
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-
-        Commode commode1 = new Commode();
-        commode1.name = "Komoda Agata ";
-        commode1.color = "Szary ";
-        commode1.price = 34.50;
-
-        Commode commode2 = new Commode();
-        commode2.name = "Komoda Beata ";
-        commode2.color = "Czerwony ";
-        commode2.price = 48.99;
+        Commode commode1 = new Commode("Komoda Agata ","Szary ",34.50);
+        Commode commode2 = new Commode("Komoda Beata ","Czerwony ",48.99);
 
         System.out.printf("Nazwa: %s, Kolor: %s, Cena: %2.2f, \n", commode1.name, commode1.color, commode1.price);
         System.out.printf("Nazwa: %s, Kolor: %s, Cena: %2.2f, \n", commode2.name, commode2.color, commode2.price);
